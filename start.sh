eeec7fe723bd51b6689db259a6f210734c1d14db
kevchaplin <kevchaplin44@gmail.com>
2023-08-08 22:54:08 +0900

    feature: dockerfile for front end

--- start.sh
+++ start.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
 if [ "$1" = "docker" ]; then
+
+# Front-end
     # Build the Docker image
     docker build -t corona-stats-api-image ./services/backend
 
     # Run a container from the built image
     docker run -d --name corona-stats-api-container -p 8080:8080 corona-stats-api-image
+
+# Back-end
+    # Build the Docker image
+    docker build -t corona-stats-client-image ./services/frontend
+
+    # Run a container from the built image
+    docker run -d --name corona-stats-client-container -p 5173:5173 corona-stats-client-image
+
 else
-    # Start backend in the background
+    # Start backend
     cd services/backend
     pip install -r requirements.txt
     uvicorn src.main:app --reload &
 
-    # Start frontend in the foreground
+    # Start frontend
     cd ../frontend
     npm install
     npm run dev
